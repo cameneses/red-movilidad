9899e9da98311d8a78e10cb74344b328415f9f9c
César Meneses <cnmeneses@uc.cl>
2023-04-04 20:37:13 -0400

    feat: add base bus stop scrapping route

--- src/controllers/busStop.js
+++ src/controllers/busStop.js
@@ -1,9 +1,46 @@
 const puppeteer = require('puppeteer');
 
-// Función de scraping
 async function getBusStopInfo(code) {
-  // Lógica de scraping utilizando Puppeteer
-  const results = { code };
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    `https://www.red.cl/planifica-tu-viaje/cuando-llega/?codsimt=${code}`,
+    { waitUntil: 'networkidle2' }
+  );
+
+  await page.waitForSelector('table.tabla-liquida.tabla-paradero');
+
+  const results = await page.$$eval(
+    'table.tabla-liquida.tabla-paradero tr',
+    (rows) => {
+      return rows
+        .map((row) => {
+          const columns = Array.from(row.querySelectorAll('td')).map(
+            (column) => column.innerText
+          );
+          const [bus, direction, status, arrival] = columns;
+
+          const index = arrival?.indexOf(' ');
+          const distance = arrival?.slice(0, index).trim();
+          const time = arrival?.slice(index + 1, arrival.length).trim();
+
+          return {
+            bus,
+            direction,
+            status,
+            distance,
+            time,
+          };
+        })
+        .filter((row) => Object.keys(row).length !== 0);
+    }
+  );
+
+  console.log(results);
+
+  await browser.close();
 
   return results;
 }
